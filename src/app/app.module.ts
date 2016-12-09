8e6038ecbb971ae576c03ecc631ea6faaa6c551f
Savpek <savolainen.pekka@gmail.com>
2016-12-09 18:35:57 +0200

    Refactored to edit module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,13 @@ import { TicketFilterComponent } from './ticket-filter/ticket-filter.component';
 import { RouterModule }   from '@angular/router';
 import { EditComponent } from './edit/edit.component';
 import { EditFormComponent } from './edit-form/edit-form.component';
-import { FilesFormComponent } from './files-form/files-form.component';
-import { ImageThumbnailComponent } from './image-thumbnail/image-thumbnail.component';
-import { FileThumbnailComponent } from './file-thumbnail/file-thumbnail.component';
-import { FileInputComponent } from './file-input/file-input.component';
-import { ExpensesFormComponent } from './expenses-form/expenses-form.component';
 import { ControlPanelComponent } from './control-panel/control-panel.component';
 import { ControlPanelUsersComponent } from './control-panel-users/control-panel-users.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { ControlPanelPartnersComponent } from './control-panel-partners/control-panel-partners.component';
-import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
 import { ControlPanelAttachedPartnersComponent } from './control-panel-attached-partners/control-panel-attached-partners.component';
 import { CxFormModule } from './shared.cxform/cxform.module';
+import { EditModule } from './edit/edit.module';
 
 @NgModule({
   declarations: [
@@ -30,18 +25,10 @@ import { CxFormModule } from './shared.cxform/cxform.module';
     NavigationComponent,
     TicketsComponent,
     TicketFilterComponent,
-    EditComponent,
-    EditFormComponent,
-    FilesFormComponent,
-    ImageThumbnailComponent,
-    FileThumbnailComponent,
-    FileInputComponent,
-    ExpensesFormComponent,
     ControlPanelComponent,
     ControlPanelUsersComponent,
     TabsComponent,
     ControlPanelPartnersComponent,
-    FeedbackFormComponent,
     ControlPanelAttachedPartnersComponent
   ],
   imports: [
@@ -49,6 +36,7 @@ import { CxFormModule } from './shared.cxform/cxform.module';
     FormsModule,
     HttpModule,
     CxFormModule,
+    EditModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/tickets', pathMatch: 'full' },
       { path: 'tickets', component: TicketsComponent },
@@ -56,13 +44,7 @@ import { CxFormModule } from './shared.cxform/cxform.module';
       { path: 'new', component: EditFormComponent},
       {
         path: 'edit/:id',
-        component: EditComponent,
-        children: [
-          { path: '', component: EditFormComponent },
-          { path: 'files', component: FilesFormComponent },
-          { path: 'feedback', component: FeedbackFormComponent },
-          { path: 'expenses', component: ExpensesFormComponent }
-        ]
+        loadChildren: () => EditModule
       },
       { path: 'new', component: EditComponent },
       {
---
+++ src/app/edit/edit.module.ts
@@ -0,0 +1,49 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { EditComponent } from './edit.component';
+import { EditFormComponent } from '../edit-form/edit-form.component';
+import { FilesFormComponent } from '../files-form/files-form.component';
+import { ExpensesFormComponent } from '../expenses-form/expenses-form.component';
+import { FeedbackFormComponent } from '../feedback-form/feedback-form.component';
+import { FileInputComponent } from '../file-input/file-input.component';
+import { FileThumbnailComponent } from '../file-thumbnail/file-thumbnail.component';
+import { ImageThumbnailComponent } from '../image-thumbnail/image-thumbnail.component';
+import { CxFormModule } from '../shared.cxform/cxform.module';
+
+const routes: any = [
+    {
+        path: 'edit/:id',
+        component: EditComponent,
+        children: [
+            { path: '', component: EditFormComponent },
+            { path: 'files', component: FilesFormComponent },
+            { path: 'feedback', component: FeedbackFormComponent },
+            { path: 'expenses', component: ExpensesFormComponent }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        CxFormModule,
+        FormsModule,
+        RouterModule.forChild(routes)],
+    declarations: [
+        FileInputComponent,
+        FileThumbnailComponent,
+        ImageThumbnailComponent,
+        EditComponent,
+        EditFormComponent,
+        FilesFormComponent,
+        FeedbackFormComponent,
+        ExpensesFormComponent
+    ],
+    exports: [
+        RouterModule
+    ],
+    providers: []
+})
+export class EditModule { }
