a376d63dcafc20ef590e578ddf0fbc95ffe7a132
Savpek <savolainen.pekka@gmail.com>
2017-07-14 14:31:57 +0300

    Fixed modal library after version update.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewContainerRef  } from '@angular/core';
 
 import { Overlay } from 'angular2-modal';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,7 @@ import { ToastsManager } from 'ng2-toastr/ng2-toastr';
   providers: []
 })
 export class AppComponent {
-    constructor(overlay: Overlay, vcRef: ViewContainerRef, private toastr: ToastsManager) {
-    overlay.defaultViewContainer = vcRef;
+    constructor(vcRef: ViewContainerRef, private toastr: ToastsManager) {
     this.toastr.setRootViewContainerRef(vcRef);
   }
 }
