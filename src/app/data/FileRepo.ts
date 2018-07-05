40eef71e6d32a5b0025a573b7b9915c83585d2f7
Savpek <savolainen.pekka@gmail.com>
2018-07-05 13:26:34 +0300

    circle ci build.

--- src/app/data/FileRepo.ts
+++ src/app/data/FileRepo.ts
@@ -5,7 +5,7 @@ import { Observable, Subject} from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Auth } from 'app/service/auth';
 import { HttpWrapper } from './httpWrapper';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class FileRepo {
@@ -27,11 +27,11 @@ export class FileRepo {
     public upload(ticketId: string, data: any): Observable<FileEntry> {
         return this.http.post(`${environment.apiBase}/file/${ticketId}`, data)
             .pipe(
-                map(response => <FileEntry>response.json()
-                .tap(entry => {
+                map(response => <FileEntry>response.json()),
+                tap(entry => {
                     this.data.push(entry);
                     this.subject.next(this.data);
-                }))
+                })
             );
     }
 
